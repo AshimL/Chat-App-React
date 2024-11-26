94bf8f62e9ddc98b2cc5b0de061c9350a1ea38da
AshimL <ashimluitel77@gmail.com>
2024-11-27 10:26:02 +1100

    Auth Secured

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider, } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjU3uH_ecY44LOlSfdWK8RrJRMssL8CAE",
-  authDomain: "chat-app-22f48.firebaseapp.com",
-  projectId: "chat-app-22f48",
-  storageBucket: "chat-app-22f48.firebasestorage.app",
-  messagingSenderId: "424954840950",
-  appId: "1:424954840950:web:93b820689fb320c248dc57"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
